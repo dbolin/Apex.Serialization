08436e108908cbe2d450a5f2c79f96fb2cf201b9
dbolin <dbolin.m@gmail.com>
2018-11-29 08:17:17 -0500

    Fix assembly attributes for .NET framework

--- Internal/Assembly.cs
+++ Internal/Assembly.cs
@@ -1,5 +1,6 @@
 ﻿
 using System.Runtime.CompilerServices;
 
+[assembly: SecurityRules(SecurityRuleSet.Level1, SkipVerificationInFullTrust = true)]
 [assembly: InternalsVisibleTo("Benchmark")]
 [assembly: InternalsVisibleTo("Apex.Serialization.Tests")]
