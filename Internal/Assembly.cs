69eda191bf562bbc7780ee5acd4269ad4ffed0df
dbolin <32645948+dbolin@users.noreply.github.com>
2018-11-28 20:35:54 -0500

    Function optimization (#1)
    * function serialization optimization first pass

--- Internal/Assembly.cs
+++ Internal/Assembly.cs
@@ -1,5 +1,7 @@
 ﻿
 using System.Runtime.CompilerServices;
+using System.Security;
 
+[assembly: SecurityCritical]
 [assembly: InternalsVisibleTo("Benchmark")]
 [assembly: InternalsVisibleTo("Apex.Serialization.Tests")]
