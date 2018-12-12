3aabc5842c675a7dd7d14e6b93ed1a3626c52183
dbolin <dbolin.m@gmail.com>
2018-12-12 15:19:29 -0500

    Add tests

--- Tests/Apex.Serialization.Tests/ConstructorTests.cs
+++ Tests/Apex.Serialization.Tests/ConstructorTests.cs
@@ -27,12 +27,35 @@ namespace Apex.Serialization.Tests
             public int Value;
         }
 
+        private class Test2
+        {
+            public Test2(bool val1 = false, bool val2 = false)
+            {
+                Val1 = val1;
+                Val2 = val2;
+            }
+            public bool Val1 { get; }
+            public bool Val2 { get; }
+        }
+
         [Fact]
         public void ConstructUsingEmpty()
         {
             var x = new Test {Value = 5};
 
             RoundTrip(x, (a, b) => { b.Value.Should().Be(0); });
+        }
+
+        [InlineData(true, false)]
+        [InlineData(false, false)]
+        [InlineData(true, true)]
+        [InlineData(false, true)]
+        [Theory]
+        public void DefaultCtorArguments(bool val1, bool val2)
+        {
+            var x = new Test2(val1, val2);
+
+            RoundTrip(x);
         }
     }
 }
