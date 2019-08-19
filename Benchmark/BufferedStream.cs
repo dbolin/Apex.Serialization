c405324d5ccde741b3dabc219a310ab1fd8f02e8
dbolin <dbolin.m@gmail.com>
2019-08-18 20:37:38 -0400

    Update version

---
+++ Benchmark/BufferedStream.cs
@@ -0,0 +1,40 @@
+﻿using Apex.Serialization.Internal;
+using BenchmarkDotNet.Attributes;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq.Expressions;
+using System.Text;
+using BufferedStream = Apex.Serialization.Internal.BufferedStream;
+
+namespace Benchmark
+{
+    public class BufferedStreamPerformance
+    {
+        private BufferedStream stream = BufferedStream.Create();
+        private MemoryStream m = new MemoryStream();
+
+        private delegate void writeSig(ref BufferedStream stream, byte b);
+        private writeSig writeByteMethod = CreateWriteByteMethod();
+
+        private static writeSig CreateWriteByteMethod()
+        {
+            var p = Expression.Parameter(typeof(BufferedStream).MakeByRefType(), "stream");
+            var b = Expression.Parameter(typeof(byte), "b");
+            var lambda = Expression.Lambda<writeSig>(Expression.Call(p, BinaryStreamMethods<BufferedStream>.GenericMethods<byte>.WriteValueMethodInfo, b), p, b);
+            var compiledLamda = lambda.Compile();
+            return (writeSig)compiledLamda;
+        }
+
+        [Benchmark]
+        public void WriteByte()
+        {
+            stream.WriteTo(m);
+
+            for(int i=0;i<1000000;++i)
+            {
+                writeByteMethod(ref stream, 1);
+            }
+        }
+    }
+}
